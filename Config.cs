f5c583866145bdcd7246dddd76dcb4b32b3285f1
RevDonz <aprilio842@gmail.com>
2022-04-16 11:14:44 +0700

    Membuat file json config dan data buku

---
+++ Config.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aplikasi_Perpustakaan
+{
+    internal class Config
+    {
+        public int height { get; set; }
+        public int width { get; set; }
+        public String bahasa { get; set; }
+
+    }
+}
