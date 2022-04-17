cac5f3ef079af69d2bf9c1b86d689e04c8171797
rpadliwinata <rpadliwinata@student.telkomuniversity.ac.id>
2022-04-17 09:17:47 +0700

    add json data reader

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -17,6 +18,19 @@ namespace Aplikasi_Perpustakaan
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+
+            //string path = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.FullName;
+            //string file = "dataBuku2.json";
+
+            //List<Buku> list_buku = new List<Buku>();
+
+            //list_buku = SerializerBuku.deserialize(path + "\\" + file);
+            //System.Diagnostics.Debug.WriteLine(path + "\\" + file);
+
+            //foreach (Buku buku in list_buku)
+            //{
+            //    System.Diagnostics.Debug.WriteLine(buku.penulis);
+            //}
         }
     }
 }
---
+++ SerializerBuku.cs
@@ -0,0 +1,33 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aplikasi_Perpustakaan
+{
+    internal class SerializerBuku
+    {
+        public static List<Buku> deserialize(string path)
+        {
+            List<Buku> list_buku = new List<Buku>();
+            //RawBuku raw = new RawBuku();
+
+            string raw_text = File.ReadAllText(path);
+
+            list_buku = JsonConvert.DeserializeObject<List<Buku>>(raw_text);
+            //raw = JsonConvert.DeserializeObject<RawBuku>(raw_text);
+            //list_buku = raw.buku;
+
+            return list_buku;
+
+        }
+    }
+
+    internal class RawBuku
+    {
+        public List<Buku> buku { get; set; }
+    }
+}
