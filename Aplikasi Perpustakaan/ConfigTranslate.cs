5155cc926180834f9bb0676b310a0db719c1d3b2
Zahrandi Rusrizal <Zahrandi@student.telkomuniversity.ac.id>
2022-06-25 16:28:22 +0700

    Perubahan naming

--- Aplikasi Perpustakaan/ConfigTranslate.cs
+++ Aplikasi Perpustakaan/ConfigTranslate.cs
@@ -5,165 +5,199 @@ namespace Aplikasi_Perpustakaan
 {
     class ConfigTranslate
     {
-        public bahasaPagePeminjaman bahasaPagePeminjaman { get; set; }
-        public bahasaPageBook bahasaPageBook { get; set; }
+        public BahasaPagePeminjaman BahasaPagePeminjaman { get; set; }
+        public BahasaPageBook BahasaPageBook { get; set; }
+        public LinkAPI LinkAPI { get; set; }
+
         public ConfigTranslate() { }
-        public ConfigTranslate(bahasaPagePeminjaman bahasaPagePeminjaman, bahasaPageBook bahasaPageBook)
+        public ConfigTranslate(BahasaPagePeminjaman bahasaPagePeminjaman, BahasaPageBook bahasaPageBook, LinkAPI linkAPI)
         {
-            this.bahasaPagePeminjaman = bahasaPagePeminjaman;
-            this.bahasaPageBook = bahasaPageBook;
+            this.BahasaPagePeminjaman = bahasaPagePeminjaman;
+            this.BahasaPageBook = bahasaPageBook;
+            this.LinkAPI = linkAPI;
         }
     }
 
-    class bahasaPagePeminjaman
+    class BahasaPagePeminjaman
     {
-        public labelIdBuku labelIdBuku { get; set; }
-        public labelNamaPeminjam labelNamaPeminjam { get; set; }
+        public LabelIdBuku LabelIdBuku { get; set; }
+        public LabelNamaPeminjam LabelNamaPeminjam { get; set; }
 
-        public bahasaPagePeminjaman() {}
-        public bahasaPagePeminjaman(labelIdBuku labelIdBuku, 
-            labelNamaPeminjam labelNamaPeminjam)
+        public BahasaPagePeminjaman() {}
+        public BahasaPagePeminjaman(LabelIdBuku labelIdBuku, 
+            LabelNamaPeminjam labelNamaPeminjam)
         {
-            this.labelIdBuku = labelIdBuku;
-            this.labelNamaPeminjam = labelNamaPeminjam;
+            this.LabelIdBuku = labelIdBuku;
+            this.LabelNamaPeminjam = labelNamaPeminjam;
         }
 
     }
 
-    class labelIdBuku
+    class LabelIdBuku
     {
-        public string id { get; set; }
-        public string en { get; set; }
+        public string Id { get; set; }
+        public string En { get; set; }
 
-        public labelIdBuku() { }
-        public labelIdBuku(string id, string en)
+        public LabelIdBuku() { }
+        public LabelIdBuku(string id, string en)
         {
-            this.id = id;
-            this.en = en;
+            this.Id = id;
+            this.En = en;
         }
     }
 
-    class labelNamaPeminjam
+    class LabelNamaPeminjam
     {
-        public string id { get; set; }
-        public string en { get; set; }
+        public string Id { get; set; }
+        public string En { get; set; }
 
-        public labelNamaPeminjam() { }
-        public labelNamaPeminjam(string id, string en)
+        public LabelNamaPeminjam() { }
+        public LabelNamaPeminjam(string id, string en)
         {
-            this.id = id;
-            this.en = en;
+            this.Id = id;
+            this.En = en;
         }
     }
 
-    class bahasaPageBook
+    class BahasaPageBook
     {
-        public labelJudulBuku labelJudulBuku { get; set; }
-        public labelPenulis labelPenulis { get; set; }
-        public labelPenerbit labelPenerbit { get; set; }
-        public labelTahunPenerbit labelTahunPenerbit { get; set; }
-        public labelJumlahHalaman labelJumlahHalaman { get; set; }
-        public buttonHapus buttonHapus { get; set; }
-        public buttonKembali buttonKembali { get; set; }
+        public LabelJudulBuku LabelJudulBuku { get; set; }
+        public LabelPenulis LabelPenulis { get; set; }
+        public LabelPenerbit LabelPenerbit { get; set; }
+        public LabelTahunPenerbit LabelTahunPenerbit { get; set; }
+        public LabelJumlahHalaman LabelJumlahHalaman { get; set; }
+        public ButtonHapus ButtonHapus { get; set; }
+        public ButtonKembali ButtonKembali { get; set; }
 
-        public bahasaPageBook() {}
-        public bahasaPageBook(labelJudulBuku labelJudulBuku, labelPenulis labelPenulis,
-            labelPenerbit labelPenerbit, labelTahunPenerbit labelTahunPenerbit,
-            labelJumlahHalaman labelJumlahHalaman, buttonHapus buttonHapus, buttonKembali
+        public BahasaPageBook() {}
+        public BahasaPageBook(LabelJudulBuku labelJudulBuku, LabelPenulis labelPenulis,
+            LabelPenerbit labelPenerbit, LabelTahunPenerbit labelTahunPenerbit,
+            LabelJumlahHalaman labelJumlahHalaman, ButtonHapus buttonHapus, ButtonKembali
             buttonKembali)
         {
-            this.labelJudulBuku = labelJudulBuku;
-            this.labelPenulis = labelPenulis;
-            this.labelPenerbit = labelPenerbit;
-            this.labelTahunPenerbit = labelTahunPenerbit;
-            this.labelJumlahHalaman = labelJumlahHalaman;
-            this.buttonHapus = buttonHapus;
-            this.buttonKembali = buttonKembali;
+            this.LabelJudulBuku = labelJudulBuku;
+            this.LabelPenulis = labelPenulis;
+            this.LabelPenerbit = labelPenerbit;
+            this.LabelTahunPenerbit = labelTahunPenerbit;
+            this.LabelJumlahHalaman = labelJumlahHalaman;
+            this.ButtonHapus = buttonHapus;
+            this.ButtonKembali = buttonKembali;
         }
     }
 
-    class labelJudulBuku
+    class LabelJudulBuku
     {
-        public string id { get; set; }
-        public string en { get; set; }
-        public labelJudulBuku() { }
-        public labelJudulBuku(string id, string en)
+        public string Id { get; set; }
+        public string En { get; set; }
+        public LabelJudulBuku() { }
+        public LabelJudulBuku(string id, string en)
         {
-            this.id= id;
-            this.en= en;
+            this.Id= id;
+            this.En= en;
         }
     }
 
-    class labelPenulis
+    class LabelPenulis
     {
-        public string id { get; set; }
-        public string en { get; set; }
-        public labelPenulis() { }
-        public labelPenulis(string id, string en)
+        public string Id { get; set; }
+        public string En { get; set; }
+        public LabelPenulis() { }
+        public LabelPenulis(string id, string en)
         {
-            this.id = id;
-            this.en = en;
+            this.Id = id;
+            this.En = en;
         }
     }
 
-    class labelPenerbit
+    class LabelPenerbit
     {
-        public string id { get; set; }
-        public string en { get; set; }
-        public labelPenerbit() { }
-        public labelPenerbit(string id, string en)
+        public string Id { get; set; }
+        public string En { get; set; }
+        public LabelPenerbit() { }
+        public LabelPenerbit(string id, string en)
         {
-            this.id = id;
-            this.en = en;
+            this.Id = id;
+            this.En = en;
         }
     }
 
-    class labelTahunPenerbit
+    class LabelTahunPenerbit
     {
-        public string id { get; set; }
-        public string en { get; set; }
-        public labelTahunPenerbit() { }
-        public labelTahunPenerbit(string id, string en)
+        public string Id { get; set; }
+        public string En { get; set; }
+        public LabelTahunPenerbit() { }
+        public LabelTahunPenerbit(string id, string en)
         {
-            this.id = id;
-            this.en = en;
+            this.Id = id;
+            this.En = en;
         }
     }
 
-    class labelJumlahHalaman
+    class LabelJumlahHalaman
     {
-        public string id { get; set; }
-        public string en { get; set; }
-        public labelJumlahHalaman() { }
-        public labelJumlahHalaman(string id, string en)
+        public string Id { get; set; }
+        public string En { get; set; }
+        public LabelJumlahHalaman() { }
+        public LabelJumlahHalaman(string id, string en)
         {
-            this.id = id;
-            this.en = en;
+            this.Id = id;
+            this.En = en;
         }
     }
 
-    class buttonHapus
+    class ButtonHapus
     {
-        public string id { get; set; }
-        public string en { get; set; }
-        public buttonHapus() { }
-        public buttonHapus(string id, string en)
+        public string Id { get; set; }
+        public string En { get; set; }
+        public ButtonHapus() { }
+        public ButtonHapus(string id, string en)
         {
-            this.id = id;
-            this.en = en;
+            this.Id = id;
+            this.En = en;
         }
     }
 
-    class buttonKembali
+    class ButtonKembali
     {
-        public string id { get; set; }
-        public string en { get; set; }
-        public buttonKembali() { }
-        public buttonKembali(string id, string en)
+        public string Id { get; set; }
+        public string En { get; set; }
+        public ButtonKembali() { }
+        public ButtonKembali(string id, string en)
         {
-            this.id = id;
-            this.en = en;
+            this.Id = id;
+            this.En = en;
         }
     }
+
+    class LinkAPI
+    {
+        public string LinkBukuGet { get; set; }
+        public string LinkBukuPost { get; set; }
+        public string LinkBukuPatch { get; set; }
+        public string LinkBukuDelete { get; set; }
+        public string LinkPeminjamanGet { get; set; }
+        public string LinkPeminjamanPost { get; set; }
+        public string LinkPeminjamanPatch { get; set; }
+        public string LinkPeminjamanDelete { get; set; }
+
+        public LinkAPI() { }
+        public LinkAPI(string linkBukuGet, string linkBukuPost, string linkBukuPatch,
+           string linkBukuDelete, string linkPeminjamanGet, string linkPeminjamanPost,
+           string linkPeminjamanPatch, string linkPeminjamanDelete)
+        {
+            this.LinkBukuGet = linkBukuGet;
+            this.LinkBukuPost = linkBukuPost;
+            this.LinkBukuPatch = linkBukuPatch;
+            this.LinkBukuDelete = linkBukuDelete;
+            this.LinkPeminjamanGet = linkPeminjamanGet;
+            this.LinkPeminjamanPost = linkPeminjamanPost;
+            this.LinkPeminjamanPatch = linkPeminjamanPatch;
+            this.LinkPeminjamanDelete = linkPeminjamanDelete;
+        }
+    }
+
+
+
+
 
 }
